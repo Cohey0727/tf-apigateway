a0650d7dbdf9670276ba494d525ae54f5a985535
Kohei <ohayousagi.ac.kook0727@gmail.com>
2021-05-23 12:34:56 +0900

    master

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
 resource "random_string" "lambda_name" {
   length  = 16
   special = false
@@ -7,27 +12,11 @@ locals {
   gateway_name = var.gateway_name == "" ? "${var.app_name}-${random_string.lambda_name.result}" : var.gateway_name
 }
 
-
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
-}
-
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name        = local.gateway_name
-  description = var.description
-}
-
-
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name               = local.gateway_name
+  description        = var.description
+  binary_media_types = ["application/octet-stream", "application/x-tar", "application/zip", "audio/basic", "audio/ogg", "audio/mp4", "audio/mpeg", "audio/wav", "audio/webm", "image/png", "image/jpg", "image/jpeg", "image/gif", "video/ogg", "video/mpeg", "video/webm"]
+  endpoint_configuration {
+    types = ["EDGE"]
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "api_gateway_id" {
-  value       = aws_api_gateway_rest_api.api_gateway.id
+  value       = aws_api_gateway_rest_api.rest_api.id
   description = "The ID of the REST API"
 }
 output "api_gateway_root_resource_id" {
-  value       = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  value       = aws_api_gateway_rest_api.rest_api.root_resource_id
   description = "The resource ID of the REST API's root"
 }
 output "api_gateway_created_date" {
-  value       = aws_api_gateway_rest_api.api_gateway.created_date
+  value       = aws_api_gateway_rest_api.rest_api.created_date
   description = "The creation date of the REST API"
 }
 output "api_gateway_execution_arn" {
-  value       = aws_api_gateway_rest_api.api_gateway.execution_arn
+  value       = aws_api_gateway_rest_api.rest_api.execution_arn
   description = "The execution ARN part to be used in lambda_permission's source_arn when allowing API Gateway to invoke a Lambda function, e.g. arn:aws:execute-api:eu-west-2:123456789012:z4675bid1j, which can be concatenated with allowed stage, method and resource path."
 }
 output "api_gateway_arn" {
-  value       = aws_api_gateway_rest_api.api_gateway.arn
+  value       = aws_api_gateway_rest_api.rest_api.arn
   description = "Amazon Resource Name (ARN)"
 }
 output "api_gateway_tags_all" {
-  value       = aws_api_gateway_rest_api.api_gateway.tags_all
+  value       = aws_api_gateway_rest_api.rest_api.tags_all
   description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block."
 }
 
